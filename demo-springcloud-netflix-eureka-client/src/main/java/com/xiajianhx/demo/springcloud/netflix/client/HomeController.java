779939b36ae951252243d458e74af83282d69db9
xiaojianhx@163.com <xiaojianhx@163.com>
2020-08-02 17:08:54 +0800

    udpate

--- demo-springcloud-netflix-eureka-client/src/main/java/com/xiajianhx/demo/springcloud/netflix/client/HomeController.java
+++ demo-springcloud-netflix-eureka-client/src/main/java/com/xiajianhx/demo/springcloud/netflix/client/HomeController.java
@@ -1,41 +1,32 @@
 package com.xiajianhx.demo.springcloud.netflix.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
 
 @Controller
 @EnableEurekaClient
-@FeignClient(value = "EUREKA-CLIENT")
 public class HomeController {
 
     private String bar;
 
     @Autowired
-    RestTemplate restTemplate;
+    private DiscoveryClient client;
 
     @ResponseBody
     @RequestMapping("/home1")
     public String hello() {
-        System.out.println(restTemplate.getForObject("http://EUREKA-CLIENT/home2", String.class));
-        return "Hello " + bar + "!";
+
+        var EUREKA_CLIENT = client.getInstances("EUREKA-CLIENT").get(0);
+        return EUREKA_CLIENT.getHost() + ":" + EUREKA_CLIENT.getPort();
     }
 
     @ResponseBody
     @RequestMapping("/home2")
     public String hello2() {
         return "Hello " + bar + "?";
-    }
-
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
